0d38373be3320a6754062820db21fe37185851db
vitalii1991 <vitalik_martov@rambler.ru>
2014-05-07 23:27:15 +0300

    Finish static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,11 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 
+RSpec.configure do |config|
+
+
+      config.include Capybara::DSL
+end
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
